ed178fc3ce1b98751b4e856fdf0f5121f708030d
Jerry Wang <ziyanwang1105@gmail.com>
2024-02-19 13:10:07 -0800

    create user table and model

---
+++ app/models/user.rb
@@ -0,0 +1,44 @@
+class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrpyt::Password.new(self.password_digest).is_passowrd?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+
+
+    private
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64(16)
+            return session_token unless User.exist?(session_token: session_token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_18_214048) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_19_204241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_18_214048) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "cat_rental_requests", "cats"
