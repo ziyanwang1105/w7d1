2ba7187b5130ec4d3a71d3ff5af5bbb660b18dcd
Jerry Wang <ziyanwang1105@gmail.com>
2024-02-19 12:36:51 -0800

    create repository

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "cats#index"
+
+  resources :cats, except: :destroy do
+    resources :cat_rental_requests, only: [:new]
+  end
+  resources :cat_rental_requests, only: [:new, :create] do
+    member do
+      post :approve
+      post :deny
+    end
+  end
+end
